b7828c38d54c0daa38c6a21f3d8c4e67c15ccc34
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2024-12-25 22:06:10 +0530

    Add examples  for the Walrus Operator in Python

---
+++ 86-Day-86-Walrus-Operator/main.py
@@ -0,0 +1,37 @@
+# a = True
+# print(a:=False)
+
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# while (n:=len(numbers)) > 0:
+#     print(numbers.pop())
+# print(n)
+
+# foods = list()
+# while True:
+#     food = input("What food do you like? ")
+#     if food == "quit":
+#         break
+#     foods.append(food)
+# print(foods)
+
+# foods = list()
+# while (food:=input("what food do you like? ")) != "quit":
+#     foods.append(food)
+# print(foods)
+
+# names = ["alok","dhiraj","niraj","Manish"]
+# if (name:=input("Enter your name:")) in names:
+#     print(f"Hello {name}")
+# else:
+#     print("Name not fouund")
+
+names = ["alok", "dhiraj", "niraj", "Manish"]
+if (name := input("Enter your name: ")) in names:
+    print(f"Hello {name}")
+else:
+    print("Name not found")
+    if input("Wanna become a member? yes/no").lower() == "yes":
+        names.append()
+        print(f"Welcome {name}, You are now a member")
+
+  
