9fe809ea5a1610b2fb6352029e3a7dc7c42dc056
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2025-01-11 18:49:15 +0530

    Add Tkinter travel form application with data submission and storage

---
+++ tkinter/9/main.py
@@ -0,0 +1,37 @@
+from tkinter import *
+
+def getvals():
+    print(f"{foodservice_value.get()}")
+
+root = Tk()
+root.geometry("644x344")
+root.title("Travel Form")
+f = "timesnewroman 11"
+Label(root,text="Welcome To Alok Travels",font="comicsansms 18 bold", pady=15).grid(row=0,column=3)
+
+name = Label(root,text="Name:",font=f).grid(row=1,column=2)
+Age = Label(root,text="Age:",font=f).grid(row=2,column=2)
+Contact = Label(root,text="Contact:",font=f).grid(row=3,column=2)
+Email = Label(root,text="Email:",font=f).grid(row=4,column=2)
+PaymentMode = Label(root,text="Payment Mode:",font=f).grid(row=5,column=2)
+
+name_value = StringVar()
+Age_value = StringVar()
+Contact_value= StringVar()
+Email_value = StringVar()
+PaymentMode_value = StringVar()
+foodservice_value = IntVar()
+
+Name_entry = Entry(root,textvariable=name_value).grid(row=1,column=3)
+Age_entry = Entry(root,textvariable=Age_value).grid(row=2,column=3)
+Contact_entry = Entry(root,textvariable=Contact_value).grid(row=3,column=3)
+Email_entry = Entry(root,textvariable=Email_value).grid(row=4,column=3)
+PaymentMode_entry = Entry(root,textvariable=PaymentMode_value).grid(row=5,column=3)
+
+food = Checkbutton(root,text="Do you want to prebook meal?",font=f,pady=15,variable=foodservice_value).grid(row=6,column=3)
+
+Button(root,text="Submit",font=f,command=getvals).grid(row=7,column=3)
+
+root.mainloop()
+
+
