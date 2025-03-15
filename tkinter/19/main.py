7a126dd955dbfc892a8ea12ac4e62a22bb463f80
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2025-03-15 19:43:48 +0530

    Fix conditional logic in login validation

---
+++ tkinter/19/main.py
@@ -0,0 +1,11 @@
+name = "python"
+# if name =="java" or name =="alok" or name =="css":
+if name =="java" or "alok" or "css":
+    print("Login successful")
+else:
+    print("login invalid")
+
+# if "hello":  
+#     print("This is True")  
+# else:  
+#     print("This is False")
